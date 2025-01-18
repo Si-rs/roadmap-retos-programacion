b7d32188cdcb5942ba2443bbf08d52661ea5b56d
Si-rs <116233548+Si-rs@users.noreply.github.com>
2025-01-18 07:00:33 -0500

    Si-rs.py
    Ejercicio 00

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Si-rs.py
@@ -0,0 +1,27 @@
+# url: https://ellibrodepython.com/
+
+#asi comentamos una linea
+
+"""Asi comentamos 
+varias lineas"""
+
+'''Asi tambien 
+comentamos varias lineas'''
+
+
+my_variable = "Mi variable"
+
+my_variable = "Nuevo valor para mi variable"
+
+# en python no existen constantes todos son variables pero por convencion escribimos 
+# la variable en mayusculas lo que indica que ese valor no se debe cambiar
+
+MY_CONSTANTE = "Mi constante"
+
+entero = 10
+flotante = 10.52
+cadena = "Mi yo"
+boleanos = True
+
+
+print("¡Hola, Python!")
